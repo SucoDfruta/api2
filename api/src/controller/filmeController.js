724e49a0e0d18dd6c0bf0a4ec6fb1a39472eb2d4
sucodfruta <noteebiel123@gmail.com>
2022-06-01 17:50:57 -0300

    mudanças 2

--- api/src/controller/filmeController.js
+++ api/src/controller/filmeController.js
@@ -1,4 +1,4 @@
-import { inserirFilme }   from "../repository/filmeRepository.js";
+import { alterarImagem, inserirFilme , listarFilmes, deletarFilme, buscarId, buscarNome}   from "../repository/filmeRepository.js";
 
 import { Router } from "express";
 
@@ -16,5 +16,72 @@ server.post('/filme', async (req, resp) => {
         })
     }
 })
+
+server.put('/filme/:id/imagem', async (req, resp) => {
+    try{
+        const { id } = req.params;
+        const resp = await alterarImagem()
+    }catch(err){
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/filme', async (req, resp) => {
+    try{
+        const resposta= await listarFilmes()
+        resp.send(resposta) 
+    }catch{
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+server.delete('/filme/:id', async (req, resp) => {
+    try{
+        const { id } = req.params
+        const resposta = await deletarFilme(id)
+        resp.send(resposta)
+    }catch(err){
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/filme/busca', async(req, resp) => {
+    try{
+        const { nome } = req.query
+        const resposta = await buscarNome(nome);
+        if(!resposta)
+        resp.status(404).send()
+        else
+        resp.send(resposta)
+    }catch(err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/filme/:id', async(req, resp) => {
+    try{
+        const id  = Number(req.params.id)
+        const resposta = await buscarId(id)
+        if(!resposta)
+        throw new Error('Filme não encontrado')
+        resp.send(resposta)
+    }catch(err) {
+
+        console.log(err);
+
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
 
 export default server;
--- api/src/repository/filmeRepository.js
+++ api/src/repository/filmeRepository.js
@@ -1,3 +1,4 @@
+
 import { con } from './connection.js'
 
 export async function inserirFilme(filme) {
@@ -7,3 +8,57 @@ export async function inserirFilme(filme) {
     filme.id = resposta.insertId
     return filme;
 }
+
+export async function alterarImagem(imagem, id) {
+    const comando = `UPDATE tb_fime
+                            SET img_filme    =?
+                            where id_filme   =?`;
+
+    const [resposta] = await con.query(comando, [imagem, id]);
+    return resposta.affectedRows;
+}
+
+export async function listarFilmes(){
+    const comando = `SELECT id_filme			id,
+                    nm_filme			nome,
+                    vl_avaliacao		avaliacao,
+                    dt_lancamento	lancamento,
+                    bt_disponivel	disponivel
+                    FROM tb_filme`;
+const [linhas] = await con.query(comando)
+return linhas;
+}
+
+export async function deletarFilme(id){
+    const comando = `DELETE FROM tb_filme 
+    WHERE id_filme = ?`
+    const [linhas] = await con.query(comando, [id])
+    return linhas;
+}
+
+export async function buscarId(id) {
+    const comando = `SELECT id_filme		id,
+                            nm_filme	    nome,
+                            vl_avaliacao	avaliacao,
+                            ds_sinopse		sinopse,
+                            dt_lancamento	lancamento,
+                            bt_disponivel	disponivel
+                    FROM    tb_filme
+                    WHERE   id_filme			= ?`
+const [linha] = await con.query(comando,[id])
+return linha[0];
+}
+
+export async function buscarNome(nome){
+    const comando = `SELECT id_filme			id,
+                    nm_filme			nome,
+                    vl_avaliacao		avaliacao,
+                    dt_lancamento	lancamento,
+                    bt_disponivel	disponivel
+                FROM tb_filme
+                WHERE nm_filme			like ?;`
+
+const [linhas] = await con.query(comando, [`%${nome}%`])
+return linhas;
+}
+
